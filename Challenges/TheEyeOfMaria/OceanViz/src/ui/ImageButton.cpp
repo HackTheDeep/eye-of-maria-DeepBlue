2fadaacc749fb8139ffb796e76c75597fa7b7e6d
Benjamin Bojko <ben@bluecadet.com>
2018-02-11 03:38:49 -0500

    Started with image buttons

---
+++ Challenges/TheEyeOfMaria/OceanViz/src/ui/ImageButton.cpp
@@ -0,0 +1,46 @@
+#include "ImageButton.h"
+
+#include "cinder/Log.h"
+
+#include "bluecadet/core/ScreenLayout.h"
+
+#include "data/OceanSettings.h"
+#include "data/TimelineManager.h"
+#include "data/DataManager.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+using namespace bluecadet::core;
+using namespace bluecadet::views;
+
+namespace amnh {
+
+ImageButton::ImageButton() {
+}
+
+ImageButton::~ImageButton() {
+}
+
+void ImageButton::setTexture(const ci::gl::TextureRef texture) {
+	mTexture = texture;
+
+	if (!mTexture) {
+		setSize(vec2(0));
+		return;
+	}
+
+	setSize(mTexture->getSize());
+}
+
+void ImageButton::draw() {
+	if (mTexture) {
+		gl::ScopedColor scopedColor(getNumTouches() > 0 ? mMouseDownColor : mMouseDownColor);
+		gl::draw(mTexture);
+	}
+}
+
+
+}
+
