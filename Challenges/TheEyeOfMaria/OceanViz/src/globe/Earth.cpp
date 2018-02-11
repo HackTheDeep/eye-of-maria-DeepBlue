225d29a8a2e52b0081c097a3f9e5848893d3c5eb
adielfernandez <adiel_fernandez@yahoo.com>
2018-02-10 22:50:21 -0500

    Add data point placeholders

--- Challenges/TheEyeOfMaria/OceanViz/src/globe/Earth.cpp
+++ Challenges/TheEyeOfMaria/OceanViz/src/globe/Earth.cpp
@@ -23,16 +23,16 @@ Earth::Earth()
 
 	// Load the textures for the Earth.
 	auto fmt = gl::Texture2d::Format().wrap( GL_REPEAT ).mipmap().minFilter( GL_LINEAR_MIPMAP_LINEAR );
-	mTexDiffuse = gl::Texture2d::create( loadImage( getAssetPath("earthDiffuse.png" ) ), fmt );
-	mTexNormal = gl::Texture2d::create( loadImage(getAssetPath("earthNormal.png" ) ), fmt );
-	mTexMask = gl::Texture2d::create( loadImage(getAssetPath("earthMask.png" ) ), fmt );
+	mTexDiffuse = gl::Texture2d::create( loadImage( getAssetPath("earth/earthDiffuse.png" ) ), fmt );
+	mTexNormal = gl::Texture2d::create( loadImage(getAssetPath("earth/earthNormal.png" ) ), fmt );
+	mTexMask = gl::Texture2d::create( loadImage(getAssetPath("earth/earthMask.png" ) ), fmt );
 
 	//mTexDiffuse = gl::Texture2d::create(loadImage(loadResource(RES_EARTHDIFFUSE)), fmt);
 	//mTexNormal = gl::Texture2d::create(loadImage(loadResource(RES_EARTHNORMAL)), fmt);
 	//mTexMask = gl::Texture2d::create(loadImage(loadResource(RES_EARTHMASK)), fmt);
 
 	// Create the Earth mesh with a custom shader.
-	auto earthShader = gl::GlslProg::create(loadAsset("earth.vert"), loadAsset("earth.frag"));
+	auto earthShader = gl::GlslProg::create(loadAsset("earth/earth.vert"), loadAsset("earth/earth.frag"));
 	earthShader->uniform( "texDiffuse", 0 );
 	earthShader->uniform( "texNormal", 1 );
 	earthShader->uniform( "texMask", 2 );
