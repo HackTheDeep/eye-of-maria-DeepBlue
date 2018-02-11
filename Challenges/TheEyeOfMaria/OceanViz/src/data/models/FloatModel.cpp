4b1c7aa0bae6c0820ef5dadbcf79d619fc9e8ffa
kevinzak <kevinJzakszewski@gmail.com>
2018-02-11 03:22:00 -0500

    Push stuff

---
+++ Challenges/TheEyeOfMaria/OceanViz/src/data/models/FloatModel.cpp
@@ -0,0 +1,11 @@
+#include "FloatModel.h"
+#include "cinder/Log.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+namespace amnh {
+	FloatModel::FloatModel() {}
+	FloatModel::~FloatModel() {}
+}
