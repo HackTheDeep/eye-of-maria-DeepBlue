4446d2325e376a53da7c8d74cd3c123514393518
Benjamin Bojko <ben@bluecadet.com>
2018-02-11 01:49:39 -0500

    Moved most code into MainController

--- Challenges/TheEyeOfMaria/OceanViz/src/MainController.cpp
+++ Challenges/TheEyeOfMaria/OceanViz/src/MainController.cpp
@@ -1,13 +1,18 @@
 #include "MainController.h"
 
 #include "cinder/Log.h"
+
+#include "bluecadet/text/StyleManager.h"
+
 #include "data/OceanSettings.h"
+#include "data/DataManager.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 using namespace bluecadet::core;
+using namespace bluecadet::text;
 using namespace bluecadet::views;
 
 namespace amnh {
@@ -18,7 +23,22 @@ MainController::MainController() {
 MainController::~MainController() {
 }
 
-void MainController::setup() {
+void MainController::setup(bluecadet::views::BaseViewRef rootView) {
+	StyleManager::getInstance()->setup(getAssetPath("fonts/styles.json"));
+
+	// Front load data
+	bool parseData = true;
+	if (parseData) {
+		DataManager::getInstance()->parseDrifterDirectoryData();
+		DataManager::getInstance()->parseDrifterData();
+	}
+
+	mUiController = make_shared<UiController>();
+	mUiController->setup();
+	rootView->addChild(mUiController);
+
+	// Create the camera controller.
+	mPov = make_shared<POV>(ci::vec3(0.0f, 0.0f, 1000.0f), ci::vec3(0.0f, 0.0f, 0.0f));
 
 	//create earth model
 	mEarth.setup();
@@ -26,20 +46,45 @@ void MainController::setup() {
 	//setup the data points
 	DataPointController::getInstance()->setup();
 	
+	//wire up signals
+	getWindow()->getSignalMouseMove().connect(bind(&MainController::handleMouseMove, this, std::placeholders::_1));
+	getWindow()->getSignalMouseWheel().connect(bind(&MainController::handleMouseWheel, this, std::placeholders::_1));
 }
 
 void MainController::update() {
+	mPov->update();
 	mEarth.update();
 	DataPointController::getInstance()->update();
 }
 
 void MainController::draw() {
-
-
+	gl::ScopedMatrices scopedMatrices;
 	gl::ScopedDepth	depth(true);
-	mEarth.draw();
-	DataPointController::getInstance()->draw();
 
+	mPov->applyMatrix();
+
+	mEarth.draw();
+
+	DataPointController::getInstance()->draw();
+}
+
+void MainController::handleMouseWheel(const ci::app::MouseEvent & event) {
+	mPov->adjustDist(event.getWheelIncrement() * -5.0f);
+}
+
+void MainController::handleMouseMove(const ci::app::MouseEvent & event) {
+	static bool firstMouseMove = true;
+
+	if (!firstMouseMove) {
+		mLastMouse = mCurrentMouse;
+	} else {
+		mLastMouse = event.getPos();
+		firstMouseMove = false;
+	}
+
+	mCurrentMouse = event.getPos();
+
+	mPov->adjustAngle((mLastMouse.x - mCurrentMouse.x) * 0.01f, mCurrentMouse.y - (getWindowHeight() * 0.5f));
 }
 
 }
