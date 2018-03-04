09d78b2d17becccfdef5bae19d973b1fd47657d0
Benjamin Bojko <ben@bluecadet.com>
2018-03-04 12:19:57 -0500

    Expanded parser script to support more CLI args and optional fields

--- Challenges/TheEyeOfMaria/OceanViz/assets/data/nc_to_json.py
+++ Challenges/TheEyeOfMaria/OceanViz/assets/data/nc_to_json.py
@@ -4,53 +4,40 @@ import json
 import netCDF4
 import numpy
 import sys
+import argparse
 
-if len(sys.argv) < 2:
-    print '\nUsage:\n'
+# Define the parser
+parser = argparse.ArgumentParser(description='NC to JSON converter')
+parser.add_argument('input', action='store', help='NC file to read from')
+parser.add_argument('output', action='store', help='JSON file to write to (defaults to <input.json>)')
+parser.add_argument('--clamp', action='store', dest='clamp', type=int, default=-1, help='Max number of samples to parse. Defaults to -1 to parse all samples.')
+parser.add_argument('--interval', action='store', dest='interval', type=int, default=-1, help='The interval used to sample every n-th element. Defaults to 1.\n')
+parser.add_argument('--fields', action='store', dest='fields', nargs='*', default=-1, help='The fields to parse from the NC file. You can use `ncdump -h` to get a list of fields.')
+args = parser.parse_args()
 
-    print '\tnc_to_json.py input.nc [output.json] [max-count]\n'
+print 'Reading NC from {}...\n'.format(args.input)
 
-    print '\t\t[output.json]: Optional filename for output json. Defaults to input.json\n'
-    print '\t\t[max-count]: The max number of samples to parse. Defaults to -1 to parse all samples.\n'
-    print '\t\t[sample-interval]: The interval used to sample every n-th element. Defaults to 0.\n'
-    sys.exit()
+ncfile = netCDF4.Dataset(args.input, 'r')
 
-inputFileName = str(sys.argv[1])
-outputFileName = str(sys.argv[2]) if len(sys.argv) >= 3 else inputFileName.replace('.nc', '.json')
-maxCount = int(sys.argv[3]) if len(sys.argv) >= 4 else -1
-sampleInterval = int(sys.argv[4]) if len(sys.argv) >= 5 else 1
+results = {}
 
-print 'Reading NC from {}...\n'.format(inputFileName)
+for field in args.fields:
+    values = ncfile.variables[field][:]
 
-ncfile = netCDF4.Dataset(inputFileName, 'r')
+    # sample every N-th events
+    if args.interval != 1:
+        values = values[::,::args.interval]
 
-times = ncfile.variables['time'][::sampleInterval]
-lats = ncfile.variables['lat'][::sampleInterval]
-lons = ncfile.variables['lon'][::sampleInterval]
-depths = ncfile.variables['z'][::sampleInterval]
+    # clamp to N events
+    if args.clamp != -1:
+        values = values[:args.clamp]
 
-if maxCount != -1:
-    times = times[:maxCount]
-    lats = lats[:maxCount]
-    lons = lons[:maxCount]
-    depths = depths[:maxCount]
-
-# remove nan and inf values
-for values in [times, lats, lons, depths]:
+    # remove nan and inf values
     values[numpy.where(numpy.isnan(values))] = 0
     values[numpy.where(numpy.isinf(values))] = 0
 
-# drifters = []
-#
-# for i in range(len(times)):
-#     drifters.append([times[i], lats[i], lons[i], depths[i]])
+    results[field] = values.tolist()
 
-with open(outputFileName, 'w') as outfile:
-    print '\nWriting JSON to {}...'.format(outputFileName)
-    # json.dump({'type': 'DrifterCollection', 'drifters': drifters}, outfile)
-    json.dump({'type': 'DrifterCollection', 'drifters': {
-        'times': times.tolist(),
-        'lats': lats.tolist(),
-        'lons': lons.tolist(),
-        'depths': depths.tolist()
-    }}, outfile)
+with open(args.output, 'w') as outfile:
+    print '\nWriting JSON to {}...'.format(args.output)
+    json.dump(results, outfile)
